689cd69f08f7853ddd1bf1714e6b900e2bcd2905
cedricfarinazzo <cedric.farinazzo@gmail.com>
2018-05-11 16:29:16 +0200

    Website: finish shared layout

---
+++ EpicoinWebSite/Epicoin/Epicoin/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Epicoin.MvcApplication" Language="C#" %>
---
+++ EpicoinWebSite/Epicoin/Epicoin/Scripts/Script.js
@@ -0,0 +1,10 @@
+﻿$(document).ready(function () {
+	(function ($) {
+		$(".dropdown-button").dropdown();
+		Materialize.updateTextFields();
+		$('select').material_select();
+		$('.materialboxed').materialbox();
+		$(".button-collapse").sideNav();
+		$('.parallax').parallax();
+	})(jQuery);
+});
